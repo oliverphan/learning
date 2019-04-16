56ed3592d96831b31a29d35a67533c0792c15de8
Oliver Phan <oliver.phan@mail.utoronto.ca>
2019-04-16 12:49:57 -0400

    valid-parentheses leetcode easy

--- Coding-Challenges/LeetCode/valid-parentheses.java
+++ Coding-Challenges/LeetCode/valid-parentheses.java
@@ -1,34 +1,41 @@
-// Rule 1: An opening can't be followed by a closing of a different
-// Rule 2: Opening brackets need to be matched by an equal number of closing of the same type
-//  + Consequence:
-
 class Solution {
+    
+    // <Key, Value>
+    private HashMap<Character, Character> mappings;
+    
+    public Solution() { // Constructor for the class
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+    
     public boolean isValid(String s) {
-        int i;
-        // Could probably improve to remove these shitty switch statements
-        for (i = 0; i < s.length() - 1; i++) {
-            // System.out.println(s.length());
-            switch (s.charAt(i)) {
+        // Utilise the stack structure
+        // The problem implies that every closing bracket encountered should have a matching             opening bracket
+        
+        //Init Stack
+        Stack<Character> stack = new Stack<Character>();
+        
+        // Iterate through the string
+        for (int i = 0; i < s.length(); i++) {
+            
+            // Case: Closing bracket
+            if (this.mappings.containsKey(c)) {
+                
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                // topElement is assigned the result of the ternary operator
+                char topElement = stack.empty() ? '#' : stack.pop();
 
-                case '(':
-                    if (s.charAt(i+1) == ']' || s.charAt(i+1) == '}') {
-                        return false;
-                    }
-                    break;
-
-                case '[':
-                    if (s.charAt(i+1) == ')' || s.charAt(i+1) == '}') {
-                        return false;
-                    }
-                    break;
-
-                case '{':
-                    if (s.charAt(i+1) == ')' || s.charAt(i+1) == ']') {
-                        return false;
-                    }
-                    break;
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // Case: opening bracket. Push to the stack
+                stack.push(c);
             }
         }
-        return true;
+        // Expression invalid if stack isn't empty here
+        return stack.isEmpty();
     }
 }
