5f3f18a00cb59479da92f0413f95635407b488fe
Oliver Phan <oliver.phan@mail.utoronto.ca>
2019-07-09 21:59:00 -0400

    updated notes

--- Javascript-Notes/arrowFunctionPractice.js
+++ Javascript-Notes/arrowFunctionPractice.js
@@ -5,13 +5,23 @@
 
 // ES6
 // const myFunc= name => {
-// 	return `Hello ${name}`;
+// 	return `Hello ${name}`; also template literals used here
 // }
 
 // Name is the parameter, after the arrow is the statements
-const myFunc= (name) => `Hello ${name}!`;
+const myFunc = (name) => `Hello ${name}!`;
 
 console.log(myFunc('Oliver'));
 
 // const a = ['tony', 'sara'];
 // console.log(a);
+
+----------------------------------------------------------
+
+`this` keyword works differently in arrow functions than regular anonymous
+functions.
+
+In arrow functions, `this` refers to the "owner" of the function (like "window")
+that created the function (where it was declared).
+
+In regular anonymous functions, `this` refers to whatever called the function.
