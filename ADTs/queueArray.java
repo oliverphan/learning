b43a15b74e2a2f00f82aa91734cfcfa95c74fb34
Oliver Phan <oliver.phan@mail.utoronto.ca>
2019-07-14 20:54:29 -0400

    main function on Java file

--- ADTs/queueArray.java
+++ ADTs/queueArray.java
@@ -3,6 +3,24 @@ public class queueArray {
     int capacity;
     int array[];
 
+    public static void main(String[] args) {
+        queueArray q = new queueArray(10);
+
+        q.enqueue(10);
+        q.enqueue(30);
+        q.enqueue(20);
+        q.enqueue(40);
+
+        System.out.println(q.dequeue() +  
+                     " dequeued from queue\n"); 
+        
+        System.out.println("Front item is " +  
+                               q.front()); 
+           
+        System.out.println("Rear item is " +  
+                                q.rear());
+    }
+
     public queueArray(int capacity) {
         this.capacity = capacity;
         this.front = this.size = 0; // the first item
