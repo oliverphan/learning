a01f68eb69cde0ce16dbca18eebec0a58172f83d
Oliver Phan <oliver.jlphan@gmail.com>
2019-07-16 00:21:52 -0400

    Initial state and props changes

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) { // this.state is to remember stuff, and initialized in constructor
+        super(props); // Always call super
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        // Use the value from the state JSObject. Change the object with setState
+        // which calls render() again, and updates child components for the hierarchy.
+        // onClick is our convienent event handler.
+        <button className="square"
+                onClick={() => this.setState({value: 'X'}) }>
+          {this.state.value} {/* value is shown on button! */}
         </button>
       );
     }
@@ -14,7 +25,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>; // value is a prop - parameters for components!
     }
   
     render() {
